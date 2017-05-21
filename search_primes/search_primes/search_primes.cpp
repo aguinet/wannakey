0f0b5a40a1c78ead7dca384b797ffca50563698f
Adrien Guinet <aguinet@quarkslab.com>
2017-05-21 10:44:38 +0200

    Reinforce licensing terms

--- search_primes/search_primes/search_primes.cpp
+++ search_primes/search_primes/search_primes.cpp
@@ -1,3 +1,20 @@
+// This file is part of wannakey.
+//
+// wannakey is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Wannakey is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with wannakey. If not, see <http://www.gnu.org/licenses/>. 2
+//
+// Copyright 2017 Adrien Guinet <adrien@guinet.me>
+
 // search_primes.cpp : Defines the entry point for the console application.
 //
 
